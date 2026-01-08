ce4e80fb2df0c22cee818b9abce80b6db5bad35c
Lucasss <78732963+lucas0522@users.noreply.github.com>
2026-01-08 12:47:42 +0800

    v2.0.4

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,13 +1,1 @@
-const config = require('flarum-webpack-config');
-const path = require('path');
-
-// 获取基础配置
-const conf = config();
-
-// 强制手动指定入口文件，不再依赖自动扫描
-conf.entry = {
-    forum: path.resolve(__dirname, 'src/forum/index.js'),
-    admin: path.resolve(__dirname, 'src/admin/index.js')
-};
-
-module.exports = conf;
+module.exports = require('flarum-webpack-config')();
